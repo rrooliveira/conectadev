5bde295201f7960075740a2405bef760e1ddb62d
rodrigo.ruyoliveira <rodrigo.oliveira.globant@estadao.com>
2021-12-23 21:26:35 -0300

    chore: initial code

---
+++ src/pages/Home/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "../../style.css"
+import Header from "../Home/components/header"
+import Main from "../Home/components/main"
+
+function Home() {
+    return (
+        <div>
+            <Header />]
+            <Main />        
+        </div>
+    )
+}
+
+export default Home
